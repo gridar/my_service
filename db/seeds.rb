835a8b87d1be0e6869fb041a672541e09b09423d
Julien Girard <julien@tymate.com>
2015-06-09 15:59:26 +0200

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    admin = User.create(email: 'admin@example.com', password: 'password')
+    admin.add_role :admin
+
+    User.create(email: 'user@example.com', password: 'password')

a95433e3082eb4b8f0dd616bd06ed027c5785917
Julien Girard <julien@tymate.com>
2015-06-09 16:00:21 +0200

    add htaccess for staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def htaccess_staging
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'my_service' && password == 'my_servicetymate'
+      end
+    end
+  end
 end
